e93b463419b4eb2425a8e77b17fbba286125dcf0
sunnode <m18205253820@163.com>
2018-08-12 13:51:28 +0800

    feat(demo): initization

---
+++ src/helloWorld/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// The entry point for the application is the 'main' function in the 'main' package.
+// The main function must take no arguments and return no values.
+func main() {
+	fmt.Println("Hello World")
+}
